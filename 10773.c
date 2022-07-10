84e98189ca323e00f24fabb7c5843e39bd78f633
hyoje099 <hyoje099@naver.com>
2022-07-10 17:43:27 +0900

    10773번 못품

--- 10773.c
+++ 10773.c
@@ -1,17 +1,31 @@
-#include<stdio.h>
+#pragma warning(disable: 4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main() {
+	int k = 0;
 
-int main(){
-	int k=0;
-	int y=0;
-	int ret=0;
-	scanf("%d",&k);
-	int i=0;
-	for(i=0;i<k;i++){
-		y=0;
-		scanf("%d",&y);
-		ret+=y;
+	int ret = 0;
+	scanf("%d", &k);
+	int y = (int*)malloc(sizeof(int) )* k;
+	int n = 0;
+	int a = 0;
+	for (int i = 0; i < k; i++) {
+
+		int t = 0;
+		scanf("%d", &t);
+		y[i] = t;
+		if (y[i] == 0) {
+			a++;
+		}
+		else {
+			for (int u = 1; u <= a; u++) {
+				ret -= y[i - u];
+			}
+			ret += y[i];
+		}
 	}
-	printf("%d",ret);
+	printf("%d", ret);
 	return 0;
 }
 
