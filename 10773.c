4922d852495517a7c89fb4325d7f73b3fc6db34a
hyoje099 <hyoje099@naver.com>
2022-07-10 17:20:48 +0900

    10773문제

--- 10773.c
+++ 10773.c
@@ -4,11 +4,12 @@ int main(){
 	int k=0;
 	int y=0;
 	int ret=0;
-	int i=0;
 	scanf("%d",&k);
+	int i=0;
 	for(i=0;i<k;i++){
+		y=0;
 		scanf("%d",&y);
-		ret=ret+y;
+		ret+=y;
 	}
 	printf("%d",ret);
 	return 0;
