00fde1a47e18cdd24474b7ef2c61f9d58139d0c2
hyoje099 <hyoje099@naver.com>
2022-08-22 22:13:24 +0900

    Update 10773.c

--- 10773.c
+++ 10773.c
@@ -2,30 +2,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+int q[10001] = { 0, };
+int res = 0;
+int push_(int a) {
+	q[res] = a;
+	res++;
+}
+int pop_() {
+	int i;
+	if (q[0] == 0 && res == 0){
+		
+	}else {
+		printf("%d\n", q[0]);
+		for (i = 0; i < res; i++) {
+			q[i] = q[i + 1];
+		}
+		q[res] = 0;
+		res--;
+	}
+}
 int main() {
 	int k = 0;
 
 	int ret = 0;
 	scanf("%d", &k);
-	int y = (int*)malloc(sizeof(int) )* k;
-	int n = 0;
-	int a = 0;
-	for (int i = 0; i < k; i++) {
+	int t=0;
+	int i;
+	for (i=0; i < k; i++) {
 
-		int t = 0;
 		scanf("%d", &t);
-		y[i] = t;
-		if (y[i] == 0) {
-			a++;
-		}
-		else {
-			for (int u = 1; u <= a; u++) {
-				ret -= y[i - u];
-			}
-			ret += y[i];
+		if(t==0){
+			pop_();
+		}else{
+			push_(t);
 		}
 	}
-	printf("%d", ret);
+	int total=0;
+	for(i=0;i<=res;i++){
+		total=total+q[i];
+	}
+	
 	return 0;
 }
 
