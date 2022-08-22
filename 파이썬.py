7642f2692603f83d34adc9f8f6267db21ff15d63
hyoje099 <hyoje099@naver.com>
2022-08-22 22:13:16 +0900

    Create 파이썬.py

---
+++ 파이썬.py
@@ -0,0 +1,24 @@
+"""x=10
+y=20
+print(x,y,sep="#")
+print()
+print("print()함수내에 end를 사용하여 띄어쓰기")
+print(x,end=' ')
+print(y,end=' ')
+
+a=int(input("입력>>"))
+print(a)
+
+
+print(type(a))
+a=10;b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)"""
+print(True and False)
+print(True or False)
+print(not(True))
